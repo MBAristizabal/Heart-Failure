7cfe06f5530c9f94a1a58f937b8efac37f75cbc1
cjcuenca <cjcuenca@gmail.com>
2022-12-21 09:47:23 +0100

    modelo de datos

---
+++ models.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class datosBD(BaseModel):
+    Age: int
+    Sex: str
+    ChestPainType: str
+    RestingBP: int
+    Cholesterol: int
+    FastingBS: int
+    RestingEGC: str
+    MaxHR: int
+    ExerciseAngina: str
+    Oldpeak: float
+    ST_Slope: str
+    HeartDisease: int
